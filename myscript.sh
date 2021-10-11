2ad9ef149c4634a2af725de096e5b00b652567f0
EC2 Default User <ec2-user@ip-172-31-36-250.ap-northeast-1.compute.internal>
2021-10-11 13:04:46 +0000

    My first shell script

---
+++ myscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hi, My name is Asad. I am working on a new git project, it includes the editing of the shell script"
+mkdir dirA
+mkdir dirB
+touch test.txt
+ls -la
+echo "This command is successful"
