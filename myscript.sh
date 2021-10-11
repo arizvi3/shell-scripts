458509424ed50444b7d716c27bd9d639a88dff5e
EC2 Default User <ec2-user@ip-172-31-36-250.ap-northeast-1.compute.internal>
2021-10-11 13:55:04 +0000

    changed the content of the file

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Hi, My name is Asad. I am working on a new git project, it includes the editing of the shell script"
-mkdir dirA
-mkdir dirB
-touch test.txt
-ls -la
+echo "Hi, My name is Asad. This is branch develop"
+rm -rf dirA
+rm -rf dirB
+touch newtest.txt
+ls
 echo "This command is successful"
 shell-scripts
